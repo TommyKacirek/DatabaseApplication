4769055ee81d27caaf9dab5e077b690198a7a298
Tomáš Kačírek <241044@vutbr.cz>
2022-12-30 18:53:58 +0000

    Update backup-skript.sh

--- backup/backup-skript.sh
+++ backup/backup-skript.sh
@@ -1,12 +1,9 @@
-
-
-# set the database name and backup directory
 DATABASE_NAME=project
 BACKUP_DIR=D:/BackUp
-# create a filename with the current date
+
 FILENAME=backup-$(date +"%Y-%m-%d").sql
 
-# use pg_dump to create a backup of the database
+
 pg_dump --format=custom --dbname=postgresql://Kaca:heslo@localhost:5432/$DATABASE_NAME -f "$BACKUP_DIR/$FILENAME" 
 
 
